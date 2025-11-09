ae4f53da00be434864567fa7343031f4555c5112
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2025-11-09 15:00:06 -0800

    Update App.tsx for MAYA Node Dashboard

---
+++ ui/web-dashboard/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>MAYA Node Dashboard</h1>
+      {/* TODO: show status, SOC, telemetry */}
+    </div>
+  );
+}
+
+export default App;
