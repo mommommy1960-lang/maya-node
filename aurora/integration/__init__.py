fd0ab4497f55196f0310d5ce72f2f03fb0dd337f
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 05:29:11 -0700

    Initialize Aurora integration package

---
+++ aurora/integration/__init__.py
@@ -0,0 +1,4 @@
+"""Simulation-only Aurora integration components."""
+from .flux_drive_bridge import AuroraCommand, AuroraFluxBridge, BridgeDecision, FluxDriveStatus
+
+__all__ = ["AuroraCommand", "AuroraFluxBridge", "BridgeDecision", "FluxDriveStatus"]
